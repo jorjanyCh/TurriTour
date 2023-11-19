8634bdcbc70102aa74b93958cae9e10893c02095
allanD2708 <106093741+allanD2708@users.noreply.github.com>
2023-11-18 22:32:27 -0600

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -137,7 +137,7 @@ https://templatemo.com/tm-559-zay-shop
     </div>
 
     <!-- Start Map -->
-    
+
     {{-- <div id="mapid" style="width: 100%; height: 300px;"></div>
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
     <script>
@@ -159,6 +159,7 @@ https://templatemo.com/tm-559-zay-shop
         mymap.scrollWheelZoom.disable();
         mymap.touchZoom.disable();
     </script> --}}
+    
     <!-- Ena Map -->
 
     <!-- Start Contact -->
