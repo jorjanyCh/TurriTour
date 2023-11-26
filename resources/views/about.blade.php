5613621c7f04fd1adb8bb8cb5235420357471954
jorjanyCh <chjorjanys@gmail.com>
2023-11-26 01:42:52 -0600

    Varios cambios en Nosotros

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -54,11 +54,16 @@ https://templatemo.com/tm-559-zay-shop
 
 
 
-    <section class="bg-success py-5">
+    <section class="bg-muted py-5">
         <div class="container">
+
             <div class="row align-items-center py-5">
-                <div class="col-md-8 text-white">
-                    <h1>Turri Tours</h1>
+                <div class="col-md-8">
+
+                    <div class="text-success">
+                        <h2>TurriTours</h2>
+                    </div>
+                    
                     <p>
                         TurriTours es una empresa que se enfoca en brindarte diferentes opciones de turismo en costa Rica,
                         especificamente en la zona de Turrialba. Turritours tiene como objetivo en dar a conocer las riqueza 
@@ -66,12 +71,226 @@ https://templatemo.com/tm-559-zay-shop
                         las opciones que ofrece esta zona del país.
                     </p>
                 </div>
-                <div class="col-md-4">
-                    <img src="assets/img/1tourss.png" alt="About Turri">
+               
+
+                <div class="row">
+                    <div class="col-lg-4 m-auto shadow"> 
+                        <div style="text-align: center; margin: 0 auto; max-width: 100%;">
+                            <img src="assets/img/cafe.png"  alt="Volcano Turri"
+                            style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
+                          </div>
+                    </div>
+
+                    <div class="col-lg-4 m-auto shadow"> 
+                          <div style="text-align: center; margin: 0 auto; max-width: 100%; col-lg-6 m-auto">
+                            <img src="assets/img/cafe.png"  alt="Volcano Turri"
+                            style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
+                          </div>
+                    </div>
+
+                    <div class="col-lg-4 m-auto shadow"> 
+                        <div style="text-align: center; margin: 0 auto; max-width: 100%; col-lg-6 m-auto">
+                          <img src="assets/img/cafe.png"  alt="Volcano Turri"
+                          style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
+                        </div>
+                  </div>
+                </div>
+            </div>
+
+
+                                        {{-- #2 Colaboraciones con locales --}}
+
+
+             <div class="row align-items-center py-5">
+                <div class="col-md-8 text-dark">
+                    <div class="text-success">
+                        <h2>Colaboración con negocios locales</h2>
+                    </div>
+                    
+                    <p>
+                        TurriTours apoya los emprendimientos locales con el fin de poder aportar a la economia 
+                        de esta zona del país, dichas colaboraciones se dará con restaurantes o sodas locales, también 
+                        con tiendas de Souvenirs meramentes locales para que el turista se pueda llevar 
+                        un recuerdo cuando visita el lugar.
+                    </p>
+                </div>
+                {{-- <div class="col-md-4">
+                    <img src=alt="About Turri">
+                </div> --}}
+
+                <div class="row">
+                    <div class="col-lg-4 m-auto shadow"> 
+                        <div style="text-align: center; margin: 0 auto; max-width: 100%;">
+                            <img src="assets/img/vturri.png"  alt="Volcano Turri"
+                            style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
+                          </div>
+                    </div>
+
+                    <div class="col-lg-4 m-auto shadow"> 
+                          <div style="text-align: center; margin: 0 auto; max-width: 100%; col-lg-6 m-auto">
+                            <img src="assets/img/vturri.png"  alt="Volcano Turri"
+                            style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
+                          </div>
+                    </div>
+
+                    <div class="col-lg-4 m-auto shadow"> 
+                        <div style="text-align: center; margin: 0 auto; max-width: 100%; col-lg-6 m-auto">
+                          <img src="assets/img/vturri.png"  alt="Volcano Turri"
+                          style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
+                        </div>
+                  </div>
+                </div>
+            </div>
+
+               {{-- #3 Otro paraffo --}}
+
+
+               <div class="row align-items-center py-5">
+                <div class="col-md-8 text-dark">
+
+
+                    <div class="text-success">
+                        <h2>Más...</h2>
+                    </div>
+                    
+                    <p>
+                        TurriTours apoya los emprendimientos locales con el fin de poder aportar a la economia 
+                        de esta zona del país, dichas colaboraciones se dará con restaurantes o sodas locales, también 
+                        con tiendas de Souvenirs meramentes locales para que el turista se pueda llevar 
+                        un recuerdo cuando visita el lugar.
+                    </p>
+                </div>
+                {{-- <div class="col-md-4">
+                    <img src=alt="About Turri">
+                </div> --}}
+
+                <div class="row">
+                    <div class="col-lg-4 m-auto shadow"> 
+                        <div style="text-align: center; margin: 0 auto; max-width: 100%;">
+                            <img src="assets/img/queso.png"  alt="Volcano Turri"
+                            style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
+                          </div>
+                    </div>
+
+                    <div class="col-lg-4 m-auto shadow"> 
+                          <div style="text-align: center; margin: 0 auto; max-width: 100%; col-lg-6 m-auto">
+                            <img src="assets/img/queso.png"  alt="Volcano Turri"
+                            style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
+                          </div>
+                    </div>
+
+                    <div class="col-lg-4 m-auto shadow"> 
+                        <div style="text-align: center; margin: 0 auto; max-width: 100%; col-lg-6 m-auto">
+                          <img src="assets/img/queso.png"  alt="Volcano Turri"
+                          style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
+                        </div>
+                  </div>
+
+
+                </div>
+            </div>
+
+
+
+           
+
+
+                     {{-- END DE PARRAFOS ACERCA DE NOSOTROS--}}
+
+
+            
+        </div>  {{-- Termona container no borrar --}}
+    </section>
+
+
+
+
+
+    <!-- Inicio codigo copiado-->
+
+
+
+
+    <div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
+            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="container">
+                    <div class="row p-5">
+                        <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+                            <img class="img-fluid" src="./assets/img/Nature.png" alt="">
+                        </div>
+                        <div class="col-lg-6 mb-0 d-flex align-items-center">
+                            <div class="text-align-left align-self-center">
+                                <h1 class="h1 text-success"><b>TurriTour</b> 
+                                    <br>
+                                <h3 class="h3">Ven y conectate con la naturaleza</h3>
+                                <p>
+                                    TurriTour Aliquam tristique nunc at semper dignissim. (beta 1). 
+                                    In quis tortor eget magna faucibus rutrum. <a rel="sponsored" class="text-success" href="https://templatemo.com" target="_blank">link1</a> website. 
+                                    Image credits go to <a rel="sponsored" class="text-success" href="https://stories.freepik.com/" target="_blank">link2</a>,
+                                    <a rel="sponsored" class="text-success" href="https://unsplash.com/" target="_blank">link3</a> and
+                                    <a rel="sponsored" class="text-success" href="https://icons8.com/" target="_blank">link4</a>.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="row p-5">
+                        <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+                            <img class="img-fluid" src="./assets/img/b2.png" alt="">
+                        </div>
+                        <div class="col-lg-6 mb-0 d-flex align-items-center">
+                            <div class="text-align-left">
+                                <h1 class="h1">Proident occaecat</h1>
+                                <h3 class="h2">Aliquip ex ea commodo consequat</h3>
+                                <p>
+                                    You are permitted to use this Zay CSS template for your commercial websites. 
+                                    You are <strong>not permitted</strong> to re-distribute the template ZIP file in any kind of template collection websites.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="row p-5">
+                        <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+                            <img class="img-fluid" src="./assets/img/b3.png" alt="">
+                        </div>
+                        <div class="col-lg-6 mb-0 d-flex align-items-center">
+                            <div class="text-align-left">
+                                <h1 class="h1">Repr in voluptate</h1>
+                                <h3 class="h2">Ullamco laboris nisi ut </h3>
+                                <p>
+                                    We bring you 100% free CSS templates for your websites. 
+                                    If you wish to support TemplateMo, please make a small contribution via PayPal or tell your friends about our website. Thank you.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </section>
+        <a class="carousel-control-prev text-decoration-none w-auto ps-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="prev">
+            <i class="fas fa-chevron-left"></i>
+        </a>
+        <a class="carousel-control-next text-decoration-none w-auto pe-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="next">
+            <i class="fas fa-chevron-right"></i>
+        </a>
+    </div>
+
+<!--  close codigo copiado-->
+
+    
     <!-- Close Banner -->
 
     <!-- Start Section -->
@@ -132,7 +351,7 @@ https://templatemo.com/tm-559-zay-shop
                        Podes visitar estos sitios asombrosos
                     </p>
                 </div>
-                <div class="col-lg-9 m-auto tempaltemo-carousel">
+                <div class="col-lg-12 m-auto tempaltemo-carousel">
                     <div class="row d-flex flex-row">
                         <!--Controls-->
                         <div class="col-1 align-self-center">
@@ -152,10 +371,10 @@ https://templatemo.com/tm-559-zay-shop
                                     <div class="carousel-item active">
                                         <div class="row">
                                             <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/bote.png" alt="Brand Logo"></a>
+                                                <a href="#"><i class="fa fa-map" aria-hidden="true"></i></a>
                                             </div>
                                             <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/bote.png" alt="Brand Logo"></a>
+                                                <a href="#"><i class="fa fa-heart" aria-hidden="true"></i></a>
                                             </div>
                                             <div class="col-3 p-md-5">
                                                 <a href="#"><img class="img-fluid brand-img" src="assets/img/bote.png" alt="Brand Logo"></a>
