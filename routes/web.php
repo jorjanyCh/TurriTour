5175a6fb2c53c2b35f884102271e86163d553806
allanD2708 <106093741+allanD2708@users.noreply.github.com>
2023-11-26 19:50:25 -0600

    inicio
    listo los inicios

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/opcion2', [controlerPrincipal::class, 'opcion2'])->name('opcion2');
 // Route::resource('/index', controlerPrincipal::class);
 //xssxss
-
+Route::get('/', function () {
+    return view('index');
+});
 
 Route::get('/index', function () {
     return view('index');
